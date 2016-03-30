463c89303b155e36b44c6e544f053108792c34fa
Francisco José Rodríguez Bogado <pacoqueen@gmail.com>
2016-03-30 22:31:16 +0200

    Corregido script de instalación.

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+cd
+if [ -d ".vim" ]; then
+    echo "Haciendo copia de seguridad del .vim actual a /tmp/dotvim.bak."
+    mv .vim /tmp/dotvim.bak
+fi
+echo "Renombrando dotvim a .vim..."
+mv dotvim .vim
+
 echo "Enlazando .vim..."
-ln -s ~/.vim/vimrc ~/.vim
+ln -s ~/.vim/vimrc ~/.vimrc
 
 echo "Instalando plugins..."
 vim +PlugInstall +qall
